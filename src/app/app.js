7490be6834f127fd445ab4101a8364351448f4a4
Exe1 <stankusl@gmail.com>
2017-02-27 11:18:26 +0000

    saving latest information

--- src/app/app.js
+++ src/app/app.js
@@ -2,7 +2,17 @@
    'use strict';
 
     // Single Page Application - just because.
-    angular.module('application', ['ui.router', 'ngMap'])
+    angular.module('application', ['ui.router', 'uiGmapgoogle-maps'])
+
+    .config( ['uiGmapGoogleMapApiProvider', function(GoogleMapApiProviders) {
+            GoogleMapApiProviders.configure({
+                key: 'AIzaSyCuCy0Mx3tj0A6AFk1oF02VIDv_rYyVEY8',
+                v: '3.28',
+                libraries: 'weather,geometry,visualization',
+                china: true
+            });
+        }]
+    )
 
     .run(['$rootScope', '$state', function($rootScope, $state) {
 
@@ -11,6 +21,14 @@
         $rootScope.WebAPI = "https://codedemon.co.uk";
 
     }])
+
+    // .config(function(uiGmapGoogleMapApiProvider) {
+    //     uiGmapGoogleMapApiProvider.configure({
+    //         key: 'AIzaSyDR5BMPcNmdr9gD8SpIPHCYBGuJNqnUrJQ',
+    //         v: '3.20',
+    //         libraries: 'weather,geometry,visualization'
+    //     });
+    // })
 
     .config(['$locationProvider', function($locationProvider) {
         $locationProvider.html5Mode(true);
--- src/app/components/home/home-controller.js
+++ src/app/components/home/home-controller.js
@@ -1,18 +1,31 @@
 (function() {
     'use strict'
-    angular.module('application').controller('HomeController', ['HomeServices', '$scope', 'NgMap', HomeController]);
+    angular.module('application').controller('HomeController', ['HomeServices', '$scope', 'uiGmapGoogleMapApi', HomeController]);
 
-    function HomeController(HomeServices, $scope, NgMap) {
+    function HomeController(HomeServices, $scope, uiGmapGoogleMapApi) {
         // Controller Variables
-        self = this;
+        var self = this;
         self.pictures = [];
         self.loading = false;
 
-        // NgMap.getMap().then(function(map) {
-        //   console.log(map.getCenter());
-        //   console.log('markers', map.markers);
-        //   console.log('shapes', map.shapes);
-        // });
+
+
+        $scope.map = {center: {latitude: 51.219053, longitude: 4.404418 }, zoom: 14 };
+        // $scope.options = {scrollwheel: false};
+
+        self.newLocationIsHome = function() {
+            $scope.map = {center: {latitude: 51.608403, longitude: -0.0708741 }, zoom: 14};
+        }
+
+        self.newLocationIsOffice = function() {
+            $scope.map = {center: {latitude: 51.5028777, longitude: -0.0864282 }, zoom: 15};
+        }
+
+        uiGmapGoogleMapApi.then(function(maps) {
+          console.log(maps);
+          // $scope.map     = { center: { latitude: 44.2126995, longitude: -100.2471641 }, zoom: 3};
+          $scope.options = { scrollwheel: false };
+        });
 
 
     };
