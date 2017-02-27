9bda8b256059d604a62cc6a8d3a353ebdb457e39
Exe1 <stankusl@gmail.com>
2017-02-27 12:01:49 +0000

    latest code

--- src/app/components/home/home-controller.js
+++ src/app/components/home/home-controller.js
@@ -8,10 +8,67 @@
         self.pictures = [];
         self.loading = false;
 
+        $scope.circles = [
+         {
+             id: 1,
+             center: {
+                 latitude: 51.608403,
+                 longitude: -0.0708741
+             },
+             radius: 500,
+             stroke: {
+                 color: '#ff590a',
+                 weight: 2,
+                 opacity: 1
+             },
+             fill: {
+                 color: '#ff590a',
+                 opacity: 0.5
+             },
+             geodesic: true, // optional: defaults to false
+             draggable: true, // optional: defaults to false
+             clickable: true, // optional: defaults to true
+             editable: true, // optional: defaults to false
+             visible: true, // optional: defaults to true
+             control: {}
+         }
+        ];
 
+       $scope.map = {center: {latitude: 40.1451, longitude: -99.6680 }, zoom: 4, bounds: {}};
+       $scope.polygons = [
+           {
+               id: 1,
+               path: [
+                   {
+                       latitude: 50,
+                       longitude: -80
+                   },
+                   {
+                       latitude: 30,
+                       longitude: -120
+                   },
+                   {
+                       latitude: 20,
+                       longitude: -95
+                   }
+               ],
+               stroke: {
+                   color: '#6060FB',
+                   weight: 3
+               },
+               editable: true,
+               draggable: true,
+               geodesic: false,
+               visible: true,
+               fill: {
+                   color: '#ff0000',
+                   opacity: 0.8
+               }
+           }
+       ];
 
-        $scope.map = {center: {latitude: 51.219053, longitude: 4.404418 }, zoom: 14 };
-        // $scope.options = {scrollwheel: false};
+        // $scope.map = {center: {latitude: 51.608403, longitude: -0.0708741 }, zoom: 14 };
+
 
         self.newLocationIsHome = function() {
             $scope.map = {center: {latitude: 51.608403, longitude: -0.0708741 }, zoom: 14};
@@ -22,8 +79,6 @@
         }
 
         uiGmapGoogleMapApi.then(function(maps) {
-          console.log(maps);
-          // $scope.map     = { center: { latitude: 44.2126995, longitude: -100.2471641 }, zoom: 3};
           $scope.options = { scrollwheel: false };
         });
 
